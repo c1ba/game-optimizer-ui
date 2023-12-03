e0ecf320341a24f5a600a7cda2757ca60c26c3c7
Costache Ioana-Brandusa-Alexandra <costachealexandra28@gmail.com>
2023-12-03 14:34:59 +0200

    First commit

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import DeviceData from './components/DeviceData.svelte';
+</script>
+
+<div class="container">
+  <DeviceData />
+</div>
+
+<style>
+  .container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 840px;
+    margin: 0 auto;
+    padding: 15px 30px 0 30px;
+  }
+</style>
